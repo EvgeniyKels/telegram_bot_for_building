acc754b11056ef88ddfd8c202c47e4be4cbab39d
Евгений Кельс <kelsem@pochtabank.ru>
2025-07-03 15:07:59 +0300

    first launch

---
+++ src/main/java/kls/tgb/TgbBot.java
@@ -0,0 +1,13 @@
+package kls.tgb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgbBot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgbBot.class);
+    }
+
+}
